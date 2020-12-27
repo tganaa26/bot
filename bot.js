4133c64a551eab2be983c979bb34acff9142f4df
tganaa26 <43716028+tganaa26@users.noreply.github.com>
2020-12-28 03:20:12 +0800

    bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NzU3OTY0MzU4MTMwMjA0NzMy.X2oDUQ.-lAcYXYLeO4bGv9KKOduXFJjcNg);//BOT_TOKEN is the Client Secret
+client.login(process.env.1VlQZnuHRlFzxjI-FjOH5pnmrkjiGlzz);//BOT_TOKEN is the Client Secret
